701afbed853116c561bc97493bab7caced758a5d
Zach <zrps3495@gmail.com>
2018-09-25 16:58:34 -0700

    Continuted Navbar implementation

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>        
+    <body>    
+        <ul id="nav">
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="">1</a>
+            </li>
+            <li>
+                <a href="">2</a>
+            </li>
+        </ul>
+        </body>
+    </html>
